116d878bba66172cf5307a3ead3feb5aa12b821f
knazir <knazir@stanford.edu>
2018-02-15 01:52:52 -0800

    Add infrastructure and welcome/goodbye handlers

---
+++ bot.js
@@ -0,0 +1,71 @@
+//////////////////// Init dependencies ////////////////////
+
+require("dotenv").config();
+
+//////////////////// Init bot ////////////////////
+
+const config = require("./config.js");
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+//////////////////// Channels ////////////////////
+
+let welcomeChannel;
+let goodbyeChannel;
+let testChannel;
+
+//////////////////// Commands ////////////////////
+
+function test(msg) {
+  const whichTest = msg.content.substr(msg.content.indexOf(" ") + 1).split(" ")[0];
+  if (whichTest === "welcome") {
+    welcome(msg.author);
+  } else if (whichTest === "goodbye") {
+    goodbye(msg.author);
+  }
+}
+
+//////////////////// Handlers ////////////////////
+
+function ready() {
+  console.log(`Logged in as ${bot.user.tag}!`);
+  welcomeChannel = bot.channels.get(config.WELCOME_CHANNEL_ID);
+  goodbyeChannel = bot.channels.get(config.GOODBYE_CHANNEL_ID);
+  testChannel = bot.channels.get(config.TEST_CHANNEL_ID);
+}
+
+// TODO: Figure out better way to handle test commands
+function handleCommand(msg) {
+  const command = msg.content.substring(config.COMMAND_PREFIX.length).split(" ")[0];
+  if (command === "test") {
+    test(msg);
+  }
+}
+
+function handleMessage(msg) {
+  if (msg.content.startsWith(config.COMMAND_PREFIX)) {
+    handleCommand(msg);
+  }
+}
+
+function welcome(member) {
+  const welcomeEmbed = new Discord.RichEmbed()
+    .setColor(2215814)
+    .addField("Welcome to Fafnir!", config.WELCOME_MESSAGE)
+    .setImage(config.BG_IMG_URL);
+  welcomeChannel.send(`Hi ${member}!`);
+  welcomeChannel.send(welcomeEmbed);
+}
+
+function goodbye(member) {
+  goodbyeChannel.send(`${member} has left the server, cya nerd :^)`);
+}
+
+//////////////////// Register handlers ////////////////////
+
+bot.on("ready", ready);
+bot.on("message", handleMessage);
+bot.on("guildMemberAdd", welcome);
+bot.on("guildMemberRemove", goodbye);
+
+bot.login(process.env.DISCORD_TOKEN);
---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  COMMAND_PREFIX: "?",
+  OPTION_PREFIX: "--",
+
+  BG_IMG_URL: "https://i.imgur.com/dL7979f.png",
+
+  WELCOME_CHANNEL_ID: "398691402139172864",
+  GOODBYE_CHANNEL_ID: "339288847894708226",
+  TEST_CHANNEL_ID: "413615322705166338",
+
+  WELCOME_MESSAGE: "Please tag Eagle or one of the online Fafnir members and tell us your IGN. " +
+  "**Failure to complete this instruction will result in a kick from the server!**"
+};
