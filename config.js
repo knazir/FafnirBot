1cc07554e5f138afd33362bf13544e9fccb30302
knazir <knazir@stanford.edu>
2018-02-16 01:22:51 -0800

    Add comments to config

--- config.js
+++ config.js
@@ -2,17 +2,20 @@ module.exports = {
   COMMAND_PREFIX: "!",
   OPTION_PREFIX: "--",
 
+  // media
   BG_IMG_URL: "https://i.imgur.com/dL7979f.png",
 
+  // channels
   WELCOME_CHANNEL_ID: "398691402139172864",
   GOODBYE_CHANNEL_ID: "413609953077624832",
   TEST_CHANNEL_ID: "413615322705166338",
 
+  // welcome and goodbye
   WELCOME_MESSAGE: "Please tag Eagle or one of the online Fafnir members and tell us your IGN. " +
   "**Failure to complete this instruction will result in a kick from the server!**",
-
   GOODBYE_MESSAGES: ["Cya nerd. :^)", "Later loser.", "Don't let the door hit you on the way out!", "Bye."],
 
+  // boss carries
   BOSS_CARRY_SHEET_URL: "https://docs.google.com/spreadsheets/d/1BaPXOM--nfYaWj1pyfQR2fqH72hC3ICEGlq84Rp2LTQ/edit#gid=0",
   BOSS_CARRY_SHEET_ID: "1BaPXOM--nfYaWj1pyfQR2fqH72hC3ICEGlq84Rp2LTQ",
   BOSS_CARRY_CARRIES_MAP: {
