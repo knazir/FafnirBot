be259ab331bf462be1915657f5e6cbb51ab2c452
knazir <knazir@stanford.edu>
2018-02-15 17:37:52 -0800

    Add read-only boss carry commands and spreadsheet integration

--- bot.js
+++ bot.js
@@ -1,18 +1,142 @@
-//////////////////// Init dependencies ////////////////////
-
-require("dotenv").config();
-
 //////////////////// Init bot ////////////////////
 
-const config = require("./config.js");
+require("dotenv").config();
+const fs = require("fs");
 const Discord = require("discord.js");
+const GoogleSpreadsheet = require('google-spreadsheet');
+const config = require("./config.js");
+
 const bot = new Discord.Client();
+
+//////////////////// Auth ////////////////////
+
+const credentials = Object.assign(require('./google-auth.json'), {
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY
+});
+
+if (process.env.NODE_ENV !== "production") {
+  credentials["private_key"] = fs.readFileSync("./secrets/google_private_key", "utf8");
+}
+
+const bossCarrySheet = new GoogleSpreadsheet(config.BOSS_CARRY_SHEET_ID);
 
 //////////////////// Channels ////////////////////
 
 let welcomeChannel;
 let goodbyeChannel;
 let testChannel;
+
+//////////////////// Generic helpers ////////////////////
+
+function rowToObject(row, mapping) {
+  const keys = Object.keys(mapping);
+  const result = {};
+  for (const key of keys) {
+    result[key] = row[mapping[key]];
+  }
+  return result;
+}
+
+//////////////////// Boss carries ////////////////////
+
+function getBossCaryRows(rawRows, limit) {
+  const rows = rawRows.map(row => {
+    const obj = rowToObject(row, config.BOSS_CARRY_CARRIES_MAP);
+    obj.date = new Date(obj.date);
+    return obj;
+  });
+  return limit ? rows.slice(0, limit) : rows;
+}
+
+function getBossesNeeded(row) {
+  return row.bossesNeeded;
+}
+
+function showBossCarriesByName(msg, limit) {
+  bossCarrySheet.useServiceAccountAuth(credentials, (err) => {
+    bossCarrySheet.getRows(1, (err, rawRows) => {
+      const rows = getBossCaryRows(rawRows, limit);
+      const carryListEmbed = new Discord.RichEmbed().setColor(3447003);
+      rows.filter(row => row.bossesNeeded)
+        .forEach(row => carryListEmbed.addField(row.name, getBossesNeeded(row)));
+      msg.reply("here is the list of carries still needed:");
+      msg.channel.send(carryListEmbed);
+    });
+  });
+}
+
+function showBossCarriesByBoss(msg, bosses) {
+  bossCarrySheet.useServiceAccountAuth(credentials, (err) => {
+    bossCarrySheet.getRows(1, (err, rawRows) => {
+      const result = {};
+
+      // check if limit was included
+      const limit = Number(bosses[bosses.length - 1]) || null;
+      if (limit) bosses = bosses.slice(0, bosses.length - 1);
+      bosses.forEach(boss => result[boss] = []);
+
+      // search for matching rows
+      const rows = getBossCaryRows(rawRows, limit);
+      rows.forEach(row => {
+        const bossesNeeded = row.bossesNeeded.split(",").map(s => s.toLowerCase().trim());
+        bosses.forEach(boss => {
+          const match = bossesNeeded.filter(s => s.indexOf(boss) !== -1)[0];
+          if (match) {
+            const extraInfo = match.substring(match.indexOf(boss) + boss.length + 1).trim();
+            result[boss].push(`${row.name} ${extraInfo}`);
+          }
+        });
+      });
+
+      // create embed
+      const carryListEmbed = new Discord.RichEmbed().setColor(3447003);
+      Object.keys(result).forEach(boss => {
+        const carriesNeeded = result[boss].join(", ") || "No carries needed!";
+        carryListEmbed.addField(boss, carriesNeeded);
+      });
+
+      // figure out reply
+      const bossPluralStr = bosses.length > 1 ? "each of those bosses" : "that boss";
+      if (limit) {
+        if (limit === 1) msg.reply(`here is the next person to be carried for ${bossPluralStr}:`);
+        else msg.reply(`here are the next ${limit} people to be carried for ${bossPluralStr}:`);
+      } else {
+        msg.reply(`here is the list of carries still needed for ${bossPluralStr}:`);
+      }
+      msg.channel.send(carryListEmbed);
+    });
+  });
+}
+
+// Assumes spreadsheet ordering is priority (does not sort by date)
+function showBossCarryList(msg) {
+  const tokens = msg.content.substring(msg.content.indexOf(" ") + 1).split(" ").slice(1);
+  if (tokens[0] === "help") {
+    msg.reply(`usage: ${config.COMMAND_PREFIX}carry list <boss name> | <number to show> | help`)
+  } else if (tokens[0] && !Number(tokens[0])) {
+    showBossCarriesByBoss(msg, tokens)
+  } else {
+    showBossCarriesByName(msg, Number(tokens[0]))
+  }
+}
+
+function showBossNameList(msg) {
+  let bossNamesList = "```";
+  Object.keys(config.BOSS_CARRY_BOSS_NAMES).forEach(boss => {
+    bossNamesList += `- ${boss} (${config.BOSS_CARRY_BOSS_NAMES[boss]})\n`;
+  });
+  bossNamesList += "\n```";
+  msg.channel.send(
+    "The current supported boss names are (case insensitive):\n" +
+    `${bossNamesList}\n` +
+    "**Make sure you use these names when entering your information in the spreadsheet!**"
+  );
+}
+
+function showBossSpreadsheet(msg) {
+  msg.reply(`the boss carry spreadsheet can be found at:\n${config.BOSS_CARRY_SHEET_URL}`);
+}
 
 //////////////////// Commands ////////////////////
 
@@ -25,13 +149,27 @@ function test(msg) {
   }
 }
 
+function handleBossCarries(msg) {
+  const tokens = msg.content.substring(msg.content.indexOf(" ") + 1).split(" ");
+  const command = tokens[0];
+  if (command === "help") {
+    msg.reply(`usage: ${config.COMMAND_PREFIX}carry list | bossnames | sheet | help`)
+  } else if (command === "list") {
+    showBossCarryList(msg);
+  } else if (command === "bossnames") {
+    showBossNameList(msg);
+  } else if (command === "sheet") {
+    showBossSpreadsheet(msg);
+  }
+}
+
 //////////////////// Handlers ////////////////////
 
 function ready() {
-  console.log(`Logged in as ${bot.user.tag}!`);
   welcomeChannel = bot.channels.get(config.WELCOME_CHANNEL_ID);
   goodbyeChannel = bot.channels.get(config.GOODBYE_CHANNEL_ID);
   testChannel = bot.channels.get(config.TEST_CHANNEL_ID);
+  console.log(`Logged in as ${bot.user.tag}!`);
 }
 
 // TODO: Figure out better way to handle test commands
@@ -40,7 +178,9 @@ function handleCommand(msg) {
   if (command === "test") {
     test(msg);
   } else if (command === "ping") {
-    msg.reply(`pong! I am currently up and running.`);
+    msg.reply(`pong! I am currently up and running in ${process.env.NODE_ENV} mode.`);
+  } else if (command === "carry") {
+    handleBossCarries(msg);
   }
 }
 
--- config.js
+++ config.js
@@ -9,5 +9,27 @@ module.exports = {
   TEST_CHANNEL_ID: "413615322705166338",
 
   WELCOME_MESSAGE: "Please tag Eagle or one of the online Fafnir members and tell us your IGN. " +
-  "**Failure to complete this instruction will result in a kick from the server!**"
+  "**Failure to complete this instruction will result in a kick from the server!**",
+
+  BOSS_CARRY_SHEET_URL: "https://docs.google.com/spreadsheets/d/1BaPXOM--nfYaWj1pyfQR2fqH72hC3ICEGlq84Rp2LTQ/edit#gid=0",
+  BOSS_CARRY_SHEET_ID: "1BaPXOM--nfYaWj1pyfQR2fqH72hC3ICEGlq84Rp2LTQ",
+  BOSS_CARRY_CARRIES_MAP: {
+    name: "name",
+    bossesNeeded: "bossneeded",
+    date: "date"
+  },
+  BOSS_CARRY_CARRIERS_MAP: {
+    carrier: "carrier",
+    soloBosses: "solobosses",
+    partyBosses: "partybosses"
+  },
+  BOSS_CARRY_BOSS_NAMES: {
+    cqueen: "Chaos Queen",
+    cpierre: "Chaos Pierre",
+    cvonbon: "Chaos Von Bon",
+    cvel: "Chaos Vellum",
+    hmag: "Hard Magnus",
+    hellux: "Hell Gollux",
+    hhilla: "Hard Hilla"
+  }
 };
